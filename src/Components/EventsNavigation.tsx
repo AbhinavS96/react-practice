5cbe18f2d765dbf409c85c2464c82d3af4f9cef9
Abhinav <abhinavsidharth@gmail.com>
2023-02-14 20:17:07 +0100

    converted to tsx

---
+++ src/Components/EventsNavigation.tsx
@@ -0,0 +1,20 @@
+import * as classes from "./EventsNavigation.module.css";
+
+const EventsNavigation: React.FC = () => {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <a href="/events">All Events</a>
+          </li>
+          <li>
+            <a href="/events/new">New Event</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default EventsNavigation;
